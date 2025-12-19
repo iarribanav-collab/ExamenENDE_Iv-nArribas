380cc509862064e8cfc34bb15ed19fe7929775f5
iarribanav <iarribanav@educacion.navarra.es>
2025-12-19 09:24:35 +0100

    metodos añadidos

--- Examen_IvanArribas/src/Examen/Alquiler.java
+++ Examen_IvanArribas/src/Examen/Alquiler.java
@@ -2,7 +2,7 @@ package Examen;
 
 public class Alquiler {
 	public static void main(String[] args) {
-		Examen_TuNombre.
+		
 	}
 }
 
--- Examen_IvanArribas/src/Examen/Vehiculo.java
+++ Examen_IvanArribas/src/Examen/Vehiculo.java
@@ -12,6 +12,37 @@ public class Vehiculo {
 		this.modelo = modelo;
 		this.prestado = prestado;
 	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public double getPrestado() {
+		return prestado;
+	}
+	public void setPrestado(double prestado) {
+		this.prestado = prestado;
+	}
+	@Override
+	public String toString() {
+		return "Vehiculo [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", prestado=" + prestado
+				+ "]";
+	}
+	
+	
 	
 	
 }
